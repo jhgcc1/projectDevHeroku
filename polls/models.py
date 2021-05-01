38528591e834c5d4cd38a9446cf19e4a11ec44db
jhgcc1 <46584477+jhgcc1@users.noreply.github.com>
2021-05-01 04:32:24 +0200

    f

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Posts(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="static/",
+    image = models.ImageField(upload_to="build/static",
         max_length=254, blank=True, null=True
     )

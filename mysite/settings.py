bf20f8af66c625f18dfbc86ab11a616f3d2e2746
jhgcc1 <46584477+jhgcc1@users.noreply.github.com>
2021-05-01 03:15:28 +0200

    f

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,8 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'mysite.urls'
 CORS_ALLOW_ALL_ORIGINS=True
 TEMPLATES = [
